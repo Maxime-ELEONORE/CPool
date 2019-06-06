9978899a36313b178cf5fbca1b80bc668d79edc7
Tydaks <maxime.eleonore@epitech.eu>
2019-06-06 10:18:35 +0200

    adding Day09

---
+++ Day09/lib/my/my_str_islower.c
@@ -0,0 +1,11 @@
+/*
+** EPITECH PROJECT, 2018
+** my_str is lower
+** File description:
+** str is lower
+*/
+
+int my_str_islower(char const *str)
+{
+    return (0);
+}
---
+++ Day09/lib/my/my_strncat.c
@@ -0,0 +1,11 @@
+/*
+** EPITECH PROJECT, 2018
+** strncat
+** File description:
+** strncat
+*/
+
+char *my_strncat(char *dest, char const *src, int nb)
+{
+    return (0);
+}
